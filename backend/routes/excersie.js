9895288271fe0a3464be0206e955291c8c46cf25
Asadullah Khan <asadullahk15@gmail.com>
2021-03-27 01:24:59 +0500

    update user and excerise

---
+++ backend/routes/excersie.js
@@ -0,0 +1,36 @@
+const router = require('express').Router();
+const { model } = require('mongoose');
+let Excersie = require('../models/excersie.model');
+// Defining Routers 
+router.route('/').get((req,res) =>{
+    Excersie.find()
+    .then(excerise => res.json(excerise))
+    .catch(err => res.status(400).json('Error' + err));
+});
+// Assigin Data to Router
+router.route('/add').post((req , res)=> {
+    const username = req.body.username;
+    const description = req.body.description;
+    const duaration =  Number(req.body.description);
+    const date = Date.parse(req.body.date);
+
+
+    const newExcerise = new Excersie(
+        {
+            username,
+            description,
+            duaration,
+            date,
+        }
+    );
+
+    newExcerise.save()
+    .then(()=> res.json('Excerise Added'))
+    .catch(err => res.status(400).json('Error' + err));
+});
+
+module.exports = router;
+
+
+});
+
