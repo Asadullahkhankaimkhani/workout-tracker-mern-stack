e9336a08c48e7df56f4fe731d738b64001d2d39e
Asadullah Khan <asadullahk15@gmail.com>
2021-04-02 17:56:06 +0500

    crush app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
-import {BrowserRouter as Router , Route} from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route} from "react-router-dom";
+
+import Navbar from "./component/navbar.jsx  "
+import ExercisesList from "./component/ex-list.jsx";
+import EditExercise from "./component/edit-excerise.jsx";
+import CreateExercise from "./component/create-excerise.jsx";
+import CreateUser from "./component/create-users.jsx";
 
 function App() {
   return (
-    <div className='container'>
-      <Router>
+    <Router>
+      <div className="container">
       <Navbar />
       <br/>
-      <Route path = "/" exact component = {ExcersiesList} />
-      <Route path = "/edit/:id"  component = {EditExcersie} />
-      <Route path = "/create"  component = {CreateExcerise} />
-      <Route path = "/user"  component = {CreateUser} />
-      </Router>
-    </div>
+      <Route path="/" exact component={ExercisesList} />
+      <Route path="/edit/:id" component={EditExercise} />
+      <Route path="/create" component={CreateExercise} />
+      <Route path="/user" component={CreateUser} />
+      </div>
+    </Router>
   );
 }
 
